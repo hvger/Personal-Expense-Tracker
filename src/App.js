980967795ed64bbc4938004eccb230b6e9ffcb58
Hristo <hvger@abv.bg>
2025-08-14 14:21:09 +0100

    debug env variable

--- src/App.js
+++ src/App.js
@@ -29,7 +29,7 @@ const ExpenseTracker = () => {
   // Load expenses from backend
   const loadExpenses = async () => {
     try {
-      const response = await fetch('http://localhost:5000/api/expenses');
+      const response = await fetch("/api/expenses");
       if (response.ok) {
         const data = await response.json();
         setExpenses(data);
@@ -57,7 +57,7 @@ const ExpenseTracker = () => {
     };
 
     try {
-      const response = await fetch('http://localhost:5000/api/expenses', {
+      const response = await fetch("/api/expenses", {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
@@ -85,7 +85,7 @@ const ExpenseTracker = () => {
   // Delete expense
   const deleteExpense = async (id) => {
     try {
-      const response = await fetch(`http://localhost:5000/api/expenses/${id}`, {
+      const response = await fetch(`api/expenses/${id}`, {
         method: 'DELETE'
       });
 
